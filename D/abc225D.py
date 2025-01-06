a7e3c10fc7b66543de4268056d6075ac39ebab78
tomi-Ryu <ryusuke.tomita.forjob@gmail.com>
2025-01-06 23:47:00 +0900

    abc225D 問題文を読み間違えると大幅な時間ロス。焦りがちだが気を付ける。

---
+++ D/abc225D.py
@@ -0,0 +1,35 @@
+N, Q = map(int, input().split())
+obj = {}
+for i in range(1, N + 1):
+  obj[i] = {"behind":-1,"ahead":-1}
+
+for _ in range(Q):
+  In = list(map(int, input().split()))
+  kind = In[0]
+  if kind <= 2:
+    x, y = In[1:]
+  else:
+    x = In[-1]
+  
+  if kind == 1:
+    obj[x]["behind"] = y
+    obj[y]["ahead"] = x
+  elif kind == 2:
+    obj[x]["behind"] = -1
+    obj[y]["ahead"] = -1
+  else:
+    refNo = x
+    while obj[refNo]["behind"] != -1:
+      refNo = obj[refNo]["behind"]
+    sentouNo = refNo
+
+    AnsArr = [1, sentouNo]
+    refNo = sentouNo
+    while obj[refNo]["ahead"] != -1:
+      refNo = obj[refNo]["ahead"]
+      AnsArr.append(refNo)
+      AnsArr[0] += 1
+    print(*AnsArr)
+
+    
+
